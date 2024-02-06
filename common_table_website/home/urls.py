91791ca504c768f9ec3eb4f3ab6e06b0e8b10293
TLancaster20 <ajl6yc@virginia.edu>
2024-02-06 10:19:57 -0500

    Work on About
    Setting up URL/view work and basic about pages templates. Plus adding some content to main about page.

--- common_table_website/home/urls.py
+++ common_table_website/home/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('about', views.about, name='about'),
     path('find', views.find, name="find"),
     path('community', views.community, name="community"),
+    path('pastoral_team', views.pastoral, name="pastoral_team"),
+    path('staff_and_council', views.staff, name="staff_and_council"),
+    path('beliefs', views.beliefs, name="beliefs"),
+    path('mission_vision_values', views.mission, names="mission_vision_values")
 ]
--- common_table_website/home/views.py
+++ common_table_website/home/views.py
@@ -9,13 +9,36 @@ def home(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
 
+
 def about(request):
     template = loader.get_template('about.html')
     return HttpResponse(template.render())
 
+
+def pastoral(request):
+    template = loader.get_template('pastoral_team.html')
+    return HttpResponse(template.render())
+
+
+def staff(request):
+    template = loader.get_template('staff_and_council.html')
+    return HttpResponse(template.render())
+
+
+def beliefs(request):
+    template = loader.get_template('beliefs.html')
+    return HttpResponse(template.render())
+
+
+def mission(request):
+    template = loader.get_template('mission_vision_values.html')
+    return HttpResponse(template.render())
+
+
 def find(request):
     template = loader.get_template('find.html')
     return HttpResponse(template.render())
+
 
 def community(request):
     template = loader.get_template('community.html')
