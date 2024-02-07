0d70a572376531ceac7fec2aa7b551b8e2668808
TLancaster20 <ajl6yc@virginia.edu>
2024-02-07 16:05:02 -0500

    Database 404
    Took program out of debug mode and made sure that website routes to 404 error page. Also set up and configured postgres database on local machine.
    
    --Figure out how to make postgres globally accessible and handle other error codes.

--- common_table_website/common_table_website/settings.py
+++ common_table_website/common_table_website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y=q)7j2=lxs234-5%q9ku=7j3)e)uc#e2zzcht6kvk2m8)4px5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'common_table_website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'common_table_website',
+        'USER': 'postgres',
+        'PASSWORD': 'suppass42',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -125,5 +129,5 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1']
 
