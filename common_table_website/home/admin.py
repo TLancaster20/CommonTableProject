468bad3f5cd492617333d6771c41c9624e277dc7
TLancaster20 <ajl6yc@virginia.edu>
2024-02-08 16:45:22 -0500

    Backend Work
    Set up admin super user as well as added model for profile for staff members.

--- common_table_website/home/admin.py
+++ common_table_website/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import StaffProfile
 
+#Admin == tonyb supuse42
 # Register your models here.
+admin.site.register(StaffProfile)
--- common_table_website/home/models.py
+++ common_table_website/home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class StaffProfile(models.Model):
+  firstname = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
+  pronouns = models.CharField(max_length=30)
+  role = models.CharField(max_length=30)
+  email = models.CharField(max_length=255)
+  bio = models.CharField(max_length=2000)
